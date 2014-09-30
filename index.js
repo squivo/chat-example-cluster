bb8508f15250b1df773f8f3c92fb80edecc81c58
Liviu Ignat <liviu@ignat.email>
2014-09-30 19:14:36 +0200

    Add cluster support

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
+var cluster = require('cluster');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var redis = require('socket.io-redis');
 
-app.get('/', function(req, res){
+io.adapter(redis({
+  host: 'localhost',
+  port: 6379
+}));
+
+var workers = process.env.WORKERS || require('os').cpus().length;
+
+app.get('/', function(req, res) {
   res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+io.on('connection', function(socket) {
+  socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+if (cluster.isMaster) {
+  console.log('start cluster with %s workers', workers - 1);
+  workers--;
+  for (var i = 0; i < workers; ++i) {
+    var worker = cluster.fork();
+    console.log('worker %s started.', worker.process.pid);
+  }
+
+  cluster.on('death', function(worker) {
+    console.log('worker %s died. restart...', worker.process.pid);
+    //cluster.fork();
+  });
+} else {
+  http.listen(3000, function() {
+    console.log('listening on *:3000');
+  });
+}
