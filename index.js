09b5bb72315ab19616a4073cc32e52aacd998c76
Liviu Ignat <iviuignat@TheSource.(none)>
2014-10-01 14:53:14 +0200

    Removed socket-io-cluster to test nodejitsu sticky session support

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
-var cluster = require('cluster');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var redis = require('redis');
-var redisAdapter = require('socket.io-redis');
 
 var port = process.env.PORT || 3000;
-var workers = process.env.WORKERS || require('os').cpus().length;
-
-var redisUrl = process.env.REDISTOGO_URL || 'redis://127.0.0.1:6379';
-var redisOptions = require('parse-redis-url')(redis).parse(redisUrl);
-var pub = redis.createClient(redisOptions.port, redisOptions.host, {
-  detect_buffers: true,
-  auth_pass: redisOptions.password
-});
-var sub = redis.createClient(redisOptions.port, redisOptions.host, {
-  detect_buffers: true,
-  auth_pass: redisOptions.password
-});
-
-io.adapter(redisAdapter({
-  pubClient: pub,
-  subClient: sub
-}));
-
-console.log('Redis adapter started with url: ' + redisUrl);
 
 app.get('/', function(req, res) {
   res.sendfile('index.html');
@@ -36,23 +14,10 @@ io.on('connection', function(socket) {
   });
 });
 
-if (cluster.isMaster) {
-  console.log('start cluster with %s workers', workers - 1);
-  workers--;
-  for (var i = 0; i < workers; ++i) {
-    var worker = cluster.fork();
-    console.log('worker %s started.', worker.process.pid);
-  }
-
-  cluster.on('death', function(worker) {
-    console.log('worker %s died. restart...', worker.process.pid);
-  });
-} else {
-  start();
-}
-
 function start() {
   http.listen(port, function() {
     console.log('listening on *:' + port);
   });
 }
+
+start();
