ab763a49f9590145e4ce6f5fd53256252d592117
Liviu Ignat <iviuignat@TheSource.(none)>
2014-10-01 11:24:53 +0200

    Add parse-redis-url

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var redis = require('redis');
 var redisAdapter = require('socket.io-redis');
-var sticky = require('sticky-session');
+
 var port = process.env.PORT || 3000;
 var workers = process.env.WORKERS || require('os').cpus().length;
 
-var pub = redis.createClient(9278, 'greeneye.redistogo.com', {
-  auth_pass: "01c7f8fdd85de3350498b978f41799ac"
+var redisUrl = process.env.REDISTOGO_URL || 'redis://127.0.0.1:6379';
+var redisOptions = require('parse-redis-url')(redis).parse(redisUrl);
+var pub = redis.createClient(redisOptions.port, redisOptions.host, {
+  auth_pass: redisOptions.password
 });
-var sub = redis.createClient(9278, 'greeneye.redistogo.com', {
+var sub = redis.createClient(redisOptions.port, redisOptions.host, {
   detect_buffers: true,
-  auth_pass: "01c7f8fdd85de3350498b978f41799ac"
+  auth_pass: redisOptions.password
 });
 
 io.adapter(redisAdapter({
@@ -49,14 +51,6 @@ if (cluster.isMaster) {
 
 function start() {
   http.listen(port, function() {
-    console.log('listening on *:' + port);
-  });
-}
-
-function stickyStart() {
-  sticky(function() {
-    return http;
-  }).listen(3000, function() {
     console.log('listening on *:' + port);
   });
 }
